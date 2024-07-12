a8ce031619df64c91dba97eaa5d9b0d2caa70014
yagneshalla <yagneshalla@gmail.com>
2024-07-12 17:52:15 +0530

    Add files via upload

---
+++ javascriptclsass1.js
@@ -0,0 +1,89 @@
+// DATA TYPES declaration and printing//
+
+let y=10;  // number//
+console.log(y)
+console.log(typeof y)
+
+let n="yagnesh";  // string //
+console.log(n)
+console.log(typeof n)
+
+let c=10;     //boolean//
+let d=20;
+let e=(c==d);
+console.log(e)
+console.log(typeof e)
+
+let f ;    // undefined//
+console.log(f) 
+console.log(typeof f)
+ 
+let x=null;  //null//
+console.log(x)
+console.log(typeof x)
+
+// mathematical operations//
+let a=10;
+let b=15;
+console.log("addition",a+b)
+console.log("subtraction",a-b)
+console.log("multiplication",a*b)
+console.log("division",a/b)
+console.log("modulo",a%b)
+
+// comparision operators//
+ let i=2;
+ let j=2;
+ let k='2';
+console.log("equals to",i==j)
+console.log("strict",i===j)
+console.log("strict",i===k)
+console.log("not equal",a!=b)
+console.log("not equal",a!==b)
+console.log("less than",a<b)
+console.log("greater than",a>b)
+console.log("less than or equal",a<=b)
+console.log("greater than or equal",a>=b)
+
+
+
+
+
+
+/* comparison operators
+    ==,===,
+
+    let x=2
+    let y='2'
+    let z=x+y
+    console.log(typeof x)
+    console.log(typeof y)
+    console.log(typeof z)
+    console.log(x==y)
+    console.log( x===y)
+    console.log( z=x+y)*/
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
